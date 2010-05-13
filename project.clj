bd506fce1822234f2b13c43d480838344d8ecaae
Justin Balthrop <justin@geni.com>
2010-05-13 14:50:23 -0700

    testing

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject classlojure "0.0.1-SNAPSHOT"
   :description "Advanced classloading for clojure."
   :namespaces [classlojure]
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [lein-clojars "0.5.0"]]
   :source-path "src/clj")
