b65435e1ec0e4bfa7abbb14542625c9d6941fe66
Justin Balthrop <justin@geni.com>
2010-05-13 15:21:04 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject classlojure "0.0.1-SNAPSHOT"
+(defproject classlojure "0.0.3-SNAPSHOT"
   :description "Advanced classloading for clojure."
   :namespaces [classlojure]
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [lein-clojars "0.5.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]]
   :source-path "src/clj")
