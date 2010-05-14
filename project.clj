d11fee396ccf2564a38639622f36961ab07c3a7c
Justin Balthrop <justin@geni.com>
2010-05-14 11:05:29 -0700

    override 'lein compile' by hand. who needs hooks?

--- project.clj
+++ project.clj
@@ -1,5 +1,19 @@
-(defproject classlojure "0.0.3-SNAPSHOT"
+(defproject classlojure "0.0.4-SNAPSHOT"
   :description "Advanced classloading for clojure."
   :namespaces [classlojure]
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]]
-  :source-path "src/clj")
+  :source-path "src/clj"
+  :java-source-path "src/jvm")
+
+(ns leiningen.compile
+  (:require lancet)
+  (:use leiningen.compile
+        [leiningen.classpath :only [make-path get-classpath]])
+  (:refer-clojure :exclude [compile]))
+
+(def compile* compile)
+(defn compile [project]
+  (lancet/javac {:srcdir    (make-path (:java-source-path project))
+                 :destdir   (:compile-path project)
+                 :classpath (apply make-path (get-classpath project))})
+  (compile* project))
