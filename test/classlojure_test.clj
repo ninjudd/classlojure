c2083d96b0606872daa12a7c4403d6a52bda8085
ninjudd <git@justinbalthrop.com>
2010-11-18 10:17:41 -0800

    add tests

---
+++ test/classlojure_test.clj
@@ -0,0 +1,34 @@
+(ns classlojure-test
+  (:use clojure.test classlojure))
+
+(deftest separate-classloader
+  (let [cl (classlojure "file:clojure-1.3.0-alpha2-SNAPSHOT.jar")]
+    (let [v (eval-in cl '*clojure-version*)]
+      (is (= 1        (:major v)))
+      (is (= 3        (:minor v)))
+      (is (= 0        (:incremental v)))
+      (is (= "alpha2" (:qualifier v))))))
+
+(deftest require-another-namespace
+  (let [cl (classlojure "file:clojure-1.3.0-alpha2-SNAPSHOT.jar")]
+    (is (= "foo-bar-baz"
+           (eval-in cl '(do
+                          (require 'clojure.string)
+                          (clojure.string/join "-" ["foo" "bar" "baz"])))))))
+
+(deftest require-srcfile
+  (let [cl (classlojure "file:test-src/" "file:clojure-1.3.0-alpha2-SNAPSHOT.jar")]
+    (is (= "foo"
+           (eval-in cl '(do
+                          (require 'foo)
+                          (foo/foo)))))))
+
+(deftest returning-nil
+  (let [cl (classlojure "file:clojure-1.3.0-alpha2-SNAPSHOT.jar")]
+    (is (= nil
+           (eval-in cl 'nil)))))
+
+(deftest eval-with-direct-forms
+  (let [cl (classlojure "file:clojure-1.3.0-alpha2-SNAPSHOT.jar")]
+    (is (= [(.hashCode System/out) (.hashCode System/err)]
+           (eval-in cl '(fn [a b] [(.hashCode a) (.hashCode b)]) System/out System/err)))))
