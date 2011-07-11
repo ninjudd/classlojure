91dc601293542c78b23b5fcfc3c265ba5edf2b5e
Justin Balthrop <git@justinbalthrop.com>
2011-07-11 12:39:51 -0700

    update useful. bump version to 0.5.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject classlojure "0.5.2"
+(defproject classlojure "0.5.3"
   :description "Advanced classloading for clojure."
   :dependencies [[clojure "1.2.0"]
-                 [useful "0.4.0"]])
+                 [useful "0.5.0"]])
