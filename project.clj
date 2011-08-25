b6c15de93f931a758c832c1cb0212c3e7f90f870
lance bradley <l@ncebradley.org>
2011-08-24 17:42:19 -0700

    v 0.5.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject classlojure "0.5.3"
+(defproject classlojure "0.5.4"
   :description "Advanced classloading for clojure."
   :dependencies [[clojure "1.2.0"]
                  [useful "0.5.0"]])
