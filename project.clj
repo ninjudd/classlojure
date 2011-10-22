b86bf5e22134165af9aa6421ed26e879a0ae4120
lance bradley <l@ncebradley.org>
2011-10-21 19:07:00 -0700

    use new useful

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject classlojure "0.6.2"
   :description "Advanced classloading for clojure."
   :dependencies [[clojure "1.2.0"]
-                 [useful "0.7.0-beta4"]])
+                 [useful "0.7.3"]])
