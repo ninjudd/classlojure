3f7d239255834c0e7abf16b79873136e61bfaf30
Justin Balthrop <git@justinbalthrop.com>
2011-11-24 08:30:05 -0800

    0.6.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject classlojure "0.6.3"
+(defproject classlojure "0.6.4"
   :description "Advanced classloading for clojure."
   :dependencies [[clojure "1.2.0"]
                  [useful "0.7.4-alpha2"]])
