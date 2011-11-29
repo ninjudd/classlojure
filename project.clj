626167469ad0eeefbbeb85116b931bc24f58ae7d
Anthony Grimes <i@raynes.me>
2011-11-29 13:58:22 -0600

    1.3ize

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject classlojure "0.6.4"
   :description "Advanced classloading for clojure."
-  :dependencies [[clojure "1.2.0"]
-                 [useful "0.7.4-alpha2"]])
+  :dependencies [[clojure "1.3.0"]
+                 [useful "0.7.5-alpha3"]])
