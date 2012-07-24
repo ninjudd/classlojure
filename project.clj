c5fdac9cfe35b063621865f071828240ffa46019
Anthony Grimes <i@raynes.me>
2012-07-24 16:46:10 -0500

    Update project.clj for lein2 and test against multiple Clojure versions.

--- project.clj
+++ project.clj
@@ -1,4 +1,13 @@
 (defproject classlojure "0.6.5"
+  :url "https://github.com/flatland/classlojure"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Advanced classloading for clojure."
-  :dependencies [[clojure "1.3.0"]
-                 [useful "0.7.5-alpha3"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [useful "0.8.3-alpha8"]]
+  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.5,default" "test"]}
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+  :repositories {"sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}})
