a45a9c807c0791f8935df7edb616299aa2e115b1
Anthony Grimes <i@raynes.me>
2013-08-27 19:11:20 -0700

    Release version 0.7.0 with recent changes and updaated deps.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject classlojure "0.6.6"
+(defproject org.flatland/classlojure "0.7.0"
   :url "https://github.com/flatland/classlojure"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Advanced classloading for clojure."
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.5,default" "test"]}
-  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :aliases {"testall" ["with-profile" "dev,default:dev,1.4,default:dev,1.6,default" "test"]}
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
   :repositories {"sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}})
